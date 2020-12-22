ac1c6a580e449bd4bba8f7e8f167edfbf30f1fdb
rafael <rafa.liotti@gmail.com>
2020-12-21 23:37:31 -0300

    start do projeto de cadastro de cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AccordionModule } from 'primeng/accordion';     //accordion and accordion tab
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TelaCadastroClienteComponent } from './pages/tela-cadastro-cliente/tela-cadastro-cliente.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaCadastroClienteComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
